1eb94e7c309ce987896f9907b30032fbfca32f00
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-19 11:31:45 +0700

    Update permissions.js

--- src/routes/permissions.js
+++ src/routes/permissions.js
@@ -1,1 +1,18 @@
 
+const express = require("express");
+const router = express.Router();
+const permissionsController = require("../controllers/permissionsController");
+
+// GET all permissions
+router.get("/", permissionsController.getAllPermissions);
+
+// POST create permission
+router.post("/", permissionsController.createPermission);
+
+// PUT update permission
+router.put("/:id", permissionsController.updatePermission);
+
+// DELETE permission
+router.delete("/:id", permissionsController.deletePermission);
+
+module.exports = router;
